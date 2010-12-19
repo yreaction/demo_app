d6fe05afde93c745966229ad34ce239ab251376d
yreaction <Yreaction@iMac-de-yreaction.local>
2010-12-19 18:21:34 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b804a17404e52bcc9298af42607a00c7acb53820dca6b00840c7a84885447456fcd20eb34aea538eab07dd49bef5208d0ef0a95360fc9ace789a6c7dd853fd08'
